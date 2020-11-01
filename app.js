84bec972ab181b136445dab40f5217a9e5e05e23
josemcepeda <josemcepedayanez@gmail.com>
2020-11-01 15:49:09 +0100

    20201101 Inclusión de Modelo de JavaScroipt para Pedir Datos e Imprimir en Página Web

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 console.log('Hola Mundo JS');
+var x=3;
+var a=3;
+var b=3;
+var c=3;
+var g=3;
+var d=3;
+var r=3;
+var l=3;
+
+var nombre;
+var edad;
+
+nombre=prompt('Ingrese su nombre:','');
+edad=prompt('Ingrese su edad:','');
+document.write('Hola ');
+document.write(nombre);
+document.write(' asi que tienes ');
+document.write(edad);
+document.write(' años');
+
 if (x>a {
     if (b>c {
         if g<d {
             if (g>r {
                 if (l<45 {
-                    
+                    alert('Mensaje de Prueba')
                 })
                 
             })
